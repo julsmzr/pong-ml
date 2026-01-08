cb9d487c47b26ee9292d72f5dffdedf2a22b74e2
NSAandYou <accounts@merke.dev>
2026-01-08 14:46:02 +0100

    - Added Comments - Move preparation functions out of training pipeline

--- src/data/mapping.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def convert_str_to_int(array: np.ndarray):
-    map = {'I': 0, 'D': 1, 'U': 2}
-    array = np.vectorize(lambda x: map.get(x, 0))(array)
-    return array
--- src/train.py
+++ src/train.py
@@ -1,27 +1,27 @@
 from data.loader import load_training_data
 from classifier.weighted_forest import WeightedForest
 from lib.distance_functions import euclidean_distance
-from data.mapping import convert_str_to_int
-from sklearn.preprocessing import MinMaxScaler
+from data.preparation import convert_str_to_int, min_max_scale, undersample
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import balanced_accuracy_score, accuracy_score, confusion_matrix
-from imblearn.under_sampling import RandomUnderSampler
 
+## Data loading
 X, y = load_training_data(random_state=42)
 X, y = X.to_numpy(), y.to_numpy()
 
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
+## Prepare data
+X = min_max_scale(X)
 y = convert_str_to_int(y)
+X, y = undersample(X, y)
 
-X, y = RandomUnderSampler().fit_resample(X,y)
-print(X.shape)
-
+## Train-Test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
+## Fit Classifier
 wf = WeightedForest(X.shape[1], 3, distance_function=euclidean_distance, accuracy_goal=0.8, initializer_low=0, initializer_high=1)
 wf.fit(X_train,y_train,100)
 
+## Test Classifier
 y_pred = wf.predict(X_test)
 print(y_pred, y_test)
 print("Accuracy: ", accuracy_score(y_test, y_pred))
